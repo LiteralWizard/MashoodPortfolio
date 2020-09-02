6fb2f184c2655811691f87e018f3dcb3c3d89bc5
LiteralWizard <mashoodrehman97@gmail.com>
2020-09-02 18:47:06 +0500

    Updates

---
+++ precache-manifest.5a1a9dfad8fcc632fa6e9f22a3eb49b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "739718f216eae7f44c816e505126c63f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4614b0e3db0295f202a",
+    "url": "/static/css/main.8408ba43.chunk.css"
+  },
+  {
+    "revision": "fd5af184df2b033d2d4f",
+    "url": "/static/js/2.0b2904a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0b2904a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4614b0e3db0295f202a",
+    "url": "/static/js/main.72dacdaf.chunk.js"
+  },
+  {
+    "revision": "0726c1fca2cb7ed6c6f9",
+    "url": "/static/js/runtime-main.12cde7c5.js"
+  },
+  {
+    "revision": "f9ff8eba14f5874e59a7a26ab62eb272",
+    "url": "/static/media/FallingWhere.f9ff8eba.mp4"
+  }
+]);
