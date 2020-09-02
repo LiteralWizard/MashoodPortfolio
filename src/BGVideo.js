82299c56a251899b450289e1bcbff09366a987ef
LiteralWizard <mashoodrehman97@gmail.com>
2020-09-02 20:17:34 +0500

    Reduced Background Video Size

--- src/BGVideo.js
+++ src/BGVideo.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import FallingWhere from './Projects/Falling.mp4'
+import FallingWhere from './Projects/FallingOP.mp4'
 
 export default function BGVideo() {
     return (
