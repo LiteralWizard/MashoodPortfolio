bf577ea5a3a4a294f4aaacab52c19dff33ca9ec7
LiteralWizard <mashoodrehman97@gmail.com>
2020-09-04 19:57:13 +0500

    Added New Functionality and BG

--- src/App.js
+++ src/App.js
@@ -1,36 +1,109 @@
 import React from 'react';
-//import styles from './App.css';
+// import styles from './App.css';
 
 import BottomWatermark from './BottomWatermark'
-import BGVideo from './BGVideo'
+// import BGVideo from './BGVideo'
+
+import FallWell from './Projects/FallWellDark.png'
+import FallMan from './Projects/FallMan.png'
+import Stairs from './Projects/StairsCycles.png'
+import PatR from './Assets/Pattern-Randomized.svg'
+
+import { Parallax } from 'react-parallax'
+import {Helmet} from "react-helmet"
 
 function App() {
   return (
-    <div>
-      <div style = {{zIndex: '-1'}}>
+    <div style = {{backgroundImage: 'url(' + PatR + ')', backgroundAttachment: 'fixed'}}>
+
+      <Helmet>
+          <meta charSet="utf-8" />
+          <title>Mashood Ur Rehman Portfolio</title>
+      </Helmet>
+
+      {/* <div style = {{zIndex: '-1'}}>
         <BGVideo/>
-      </div>
+      </div> */}
+
+      {/* <div style = {{width: '100%', display: 'flex', justifyContent: 'center'}}>
+        <img src = {FallMan} alt = '' style = {{
+          zIndex: '2',
+          position: 'fixed',
+          height: '30% ',
+          bottom: '5%',
+        }}/>
+      </div> */}
+
+      <Parallax
+          blur = {{min: -1, max: 5}}
+          bgImage = {FallWell}
+          bgImageAlt = ''
+          strength = {500}
+          style = {{boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'}}>
+
+        <div style = {{
+          display: 'grid',
+          placeItems: 'center',
+          height: '100vh',
+          color: 'white',
+          textShadow: '2px 2px 5px #000',
+          zIndex: '0',
+        }}>
+          <div style = {{
+            display: "flex",
+            flexDirection: 'column',
+            alignItems: "center",
+            width: "30%",
+            minWidth: '400px',
+            textAlign: "center",
+          }}>
+            <span style = {{fontFamily: 'Oswald', fontWeight: '200', fontSize: '5em'}}>Hello There</span><br/>
+            <span style = {{fontWeight: '400', fontSize: '1.6em'}}>I'm Mashood Ur Rehman</span>
+          </div>
+        </div>
+
+      </Parallax>
 
       <div style = {{
-        display: 'grid',
-        placeItems: 'center',
-        height: '100vh',
         color: 'white',
-        fontFamily: 'Oswald',
-        zIndex: '0',
-      }}>
-        <div style = {{
-          display: "flex",
-          flexDirection: 'column',
-          alignItems: "center",
-          width: "30%",
-          minWidth: '400px',
-          textAlign: "center",
-        }}>
-          <span style = {{fontWeight: '200', fontSize: '5em'}}>Hello There</span><br/>
-          <span style = {{fontWeight: '400', fontSize: '2.1em'}}>I'm Mashood Ur Rehman</span>
+        fontWeight: '300',
+        fontSize: '2em',
+        display: 'flex',
+        justifyContent: 'center'}}><span>|  |  |</span></div>
+
+      <Parallax 
+        bgImage = {Stairs}
+        blur = {{}}
+        strength = {-300}
+        style = {{boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'}}
+        renderLayer = {percentage => (
+        <div style = {{width: '100%', display: 'flex', justifyContent: 'center'}}>
+          <img src = {FallMan} alt = '' style = {{
+            zIndex: '2',
+            position: 'fixed',
+            height: '30% ',
+            bottom: '5%',
+            transform: `rotate(${percentage * 20}deg)`,
+            opacity: `${1 - percentage * 2}`,
+          }}/>
+
+          <div style = {{
+            paddingTop: '20vh',
+            paddingBottom: '20vh',
+            zIndex: '0',
+            textAlign: 'center',
+            textShadow: '2px 2px 5px #000',
+            color: 'white',
+            fontWeight: '200',
+            fontSize: '2em',
+          }}>
+            <span>Computer Scientist</span><br/><br/>
+            <span>Designer</span><br/><br/>
+            <span>3D Artist</span>
+          </div>
         </div>
-      </div>
+      )}>
+      </Parallax>
 
       <div style = {{zIndex: '2'}}>
         <BottomWatermark/>
